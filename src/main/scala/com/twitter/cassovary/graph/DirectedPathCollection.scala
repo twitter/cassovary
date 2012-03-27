629aab35cb7c414432bcb452b024b09da51074e6
Ning Liang <ning@twitter.com>
2012-03-27 16:26:20 -0700

    in progress directed path collection

--- src/main/scala/com/twitter/cassovary/graph/DirectedPathCollection.scala
+++ src/main/scala/com/twitter/cassovary/graph/DirectedPathCollection.scala
@@ -13,12 +13,14 @@
  */
 package com.twitter.cassovary.graph
 
-import collection.mutable
+import it.unimi.dsi.fastutil.Arrays
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
+import it.unimi.dsi.fastutil.ints.{Int2ObjectOpenHashMap, IntComparator}
 
 
 /**
  * Represents a collection of directed paths. For example, a collection of paths out of
- * the same source node.
+ * the same source node. This class is *not* thread safe!
  */
 class DirectedPathCollection {
 
@@ -26,15 +28,11 @@ class DirectedPathCollection {
   private val currPath = DirectedPath.builder()
 
   // pathCountsPerId(id)(path) = count of #times path has been seen ending in id
-  private val pathCountsPerId = new mutable.HashMap[Int, mutable.HashMap[DirectedPath, Int]] {
-    override def default(node: Int) = {
-      val h1 = new mutable.HashMap[DirectedPath, Int] {
-        override def default(p: DirectedPath) = 0
-      }
-      put(node, h1)
-      h1
-    }
-  }
+  private val pathCountsPerId = new Int2ObjectOpenHashMap[Object2IntOpenHashMap[DirectedPath]]
+
+  // int comparator and swapper for sorting directed path
+  private val directedPathIntComparator = new DirectedPathIntComparator
+  private val directedPathArraySwapper = new DirectedPathArraySwapper
 
   /**
    * Appends node to the current path and record this path against the node.
@@ -42,7 +40,7 @@ class DirectedPathCollection {
    */
   def appendToCurrentPath(node: Int) {
     currPath.append(node)
-    pathCountsPerId(node)(currPath.snapshot) += 1
+    pathCountsPerIdWithDefault(node).add(currPath.snapshot, 1)
   }
 
   /**
@@ -57,9 +55,20 @@ class DirectedPathCollection {
    *         ending at {@code node} is observed
    */
   def topPathsTill(node: Int, num: Int) = {
-    val paths = pathCountsPerId(node)
-    val top = paths.toList.sortBy { case (path, count) => (-count, path.length) }
-    top.take(num)
+    val pathCounterMap = pathCountsPerIdWithDefault(node)
+    val pathCount = pathCounterMap.size
+    val pathArray = new Array[DirectedPath](pathCount)
+
+    directedPathIntComparator.pathCounterMap = pathCounterMap
+    directedPathIntComparator.pathArray = pathArray
+    directedPathArraySwapper.pathArray = pathArray
+
+    val pathArray = new Array[DirectedPath](pathCount)
+    pathCounterMap.keySet.toArray(pathArray)
+    Arrays.quicksort(0, pathCount, directedPathIntComparator, directedPathArraySwapper)
+
+    // TODO make this an array of ... tuples?
+    pathArray.toSeq.take(num).map { path => (path, pathCounterMap.getInt(path)) }
   }
 
   /**
@@ -75,7 +84,7 @@ class DirectedPathCollection {
   /**
    * @return the total number of unique paths that end at {@code node}
    */
-  def numUniquePathsTill(node: Int) = pathCountsPerId(node).size
+  def numUniquePathsTill(node: Int) = pathCountsPerIdWithDefault(node).size
 
   /**
    * @return the total number of unique paths in this collection
@@ -84,4 +93,33 @@ class DirectedPathCollection {
     pathCountsPerId.keysIterator.foldLeft(0) { case (tot, node) => tot + numUniquePathsTill(node) }
   }
 
+  private def pathCountsPerIdWithDefault(node: Int) = {
+    if (!pathCountsPerId.containsKey(node)) {
+      val map = new Object2IntOpenHashMap[DirectedPath]
+      pathCountsPerId.put(node, map)
+    }
+    pathCountsPerId.get(node)
+  }
+
+}
+
+class DirectedPathIntComparator extends IntComparator {
+  var pathCounterMap
+  var pathArray
+
+  override def compare(a: Int, b: Int) = {
+    val aCount = pathCounterMap.getInt(pathArray(a))
+    val bCount = pathCounterMap.getInt(pathArray(b))
+    bCount - aCount
+  }
+}
+
+class DirectedPathArraySwapper extends Swapper {
+  var pathArray
+
+  override def swap(a: Int, b: Int) {
+    val temp = pathArray(a)
+    pathArray(a) = pathArray(b)
+    pathArray(b) = temp
+  }
 }
