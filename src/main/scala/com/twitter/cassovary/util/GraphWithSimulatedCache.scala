1e354e074101b3686f2003c48038d11a15d57014
Justin Cheng <jcheng@twitter.com>
2012-07-07 23:21:57 -0700

    Updated GraphWithSimulatedCache

--- src/main/scala/com/twitter/cassovary/util/GraphWithSimulatedCache.scala
+++ src/main/scala/com/twitter/cassovary/util/GraphWithSimulatedCache.scala
@@ -1,13 +1,20 @@
 package com.twitter.cassovary.util
 
-import com.twitter.cassovary.graph.Graph
+import com.twitter.cassovary.graph.{DirectedGraph, Graph}
+import java.io.File
 
 class GraphWithSimulatedCache(val g: Graph, val cacheSize: Int, val cacheMechanism: String) extends Graph {
 
   val cache = cacheMechanism match {
-    case "clock" => new ClockSimulatedCache(cacheSize)
+    case "clock" => {
+      val dg = g.asInstanceOf[DirectedGraph]
+      new ClockSimulatedCache(dg.maxNodeId, cacheSize)
+    }
     case "mru" => new MRUSimulatedCache(cacheSize)
-    case _ => new LRUSimulatedCache(cacheSize)
+    case _ => {
+      val dg = g.asInstanceOf[DirectedGraph]
+      new FastLRUSimulatedCache(dg.maxNodeId, cacheSize)
+    }
   }
 
   override def getNodeById(id: Int) = {
@@ -23,8 +30,19 @@ class GraphWithSimulatedCache(val g: Graph, val cacheSize: Int, val cacheMechani
     cache.getStats(verbose)
   }
 
+  def writeStats(fileName: String) = {
+    val (misses, accesses, missRatio) = cache.getStats()
+    printToFile(new File(fileName))(p => {
+      p.println("%s\t%s\t%s".format(misses, accesses, missRatio))
+    })
+  }
+
   def graph = {
     g
   }
 
+  private def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f)
+    try { op(p) } finally { p.close() }
+  }
 }
