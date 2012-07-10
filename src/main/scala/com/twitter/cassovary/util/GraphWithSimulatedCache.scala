cf7f877fc89c5fe9e9e319d88dcf5b617233139b
Justin Cheng <jcheng@twitter.com>
2012-07-10 09:56:09 -0700

    Bug fix?

--- src/main/scala/com/twitter/cassovary/util/GraphWithSimulatedCache.scala
+++ src/main/scala/com/twitter/cassovary/util/GraphWithSimulatedCache.scala
@@ -6,25 +6,6 @@ import net.lag.logging.Logger
 import com.twitter.ostrich.stats.Stats
 import com.twitter.cassovary.graph.GraphDir
 
-class GraphWithSimulatedVarCache(g: Graph, cacheSize: Int, cacheMechanism: String, statsInterval: Int)
-	extends GraphWithSimulatedCache(g, cacheSize, cacheMechanism, statsInterval) {
-  override def getNodeById(id: Int) = {
-    val node = g.getNodeById(id)
-    node match {
-      case Some(n) => {
-        cache.get(id, n.neighborCount(GraphDir.OutDir))
-        if (cache.accesses % statsInterval == 0) {
-	      val (m, a, r) = cache.getStats
-	      Stats.addMetric("cache_misses", m.toInt)
-	      Stats.addMetric("cache_accesses", a.toInt)
-	      log.info("simcache interval %s %s %s".format(m, a, r))
-	    }
-      }
-    }
-    node
-  }
-}
-
 class GraphWithSimulatedCache(val g: Graph, val cacheSize: Int, val cacheMechanism: String, val statsInterval: Int) extends Graph {
   
   protected val log = Logger.get
@@ -79,5 +60,24 @@ class GraphWithSimulatedCache(val g: Graph, val cacheSize: Int, val cacheMechani
   private def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
     val p = new java.io.PrintWriter(f)
     try { op(p) } finally { p.close() }
+  }
+}
+
+class GraphWithSimulatedVarCache(g: Graph, cacheSize: Int, cacheMechanism: String, statsInterval: Int)
+  extends GraphWithSimulatedCache(g, cacheSize, cacheMechanism, statsInterval) {
+  override def getNodeById(id: Int) = {
+    val node = g.getNodeById(id)
+    node match {
+      case Some(n) => {
+        cache.get(id, n.neighborCount(GraphDir.OutDir))
+        if (cache.accesses % statsInterval == 0) {
+          val (m, a, r) = cache.getStats
+          Stats.addMetric("cache_misses", m.toInt)
+          Stats.addMetric("cache_accesses", a.toInt)
+          log.info("simcache interval %s %s %s".format(m, a, r))
+        }
+      }
+    }
+    node
   }
 }
