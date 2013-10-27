1d82f80561990d84283e70c28f340db87e701420
VinodKumarLogan <vinodkumarlogan@gmail.com>
2013-10-27 22:20:17 +0530

    porting to scala 2.10.0

--- src/test/scala/com/twitter/cassovary/graph/SynchronizedDynamicGraphSpec.scala
+++ src/test/scala/com/twitter/cassovary/graph/SynchronizedDynamicGraphSpec.scala
@@ -1,16 +1,16 @@
 /*
- * Copyright 2012 Twitter, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
- * file except in compliance with the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
+* Copyright 2012 Twitter, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+* file except in compliance with the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed
+* under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+* CONDITIONS OF ANY KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations under the License.
+*/
 package com.twitter.cassovary.graph
 
 import org.specs.Specification
@@ -78,20 +78,33 @@ class SynchronizedDynamicGraphSpec extends Specification {
   }
 
   "multipe threads add nodes/edge simultaneously" in {
-    import akka.actor.Actor
-    import akka.actor.Actor._
+    import akka.actor._
+    import akka.actor.ActorDSL._
+    import akka.pattern.ask    
+    import akka.util.Timeout
+    import scala.concurrent._
+    import scala.concurrent.duration._
 
+    val system = ActorSystem("migration-system")
     val graph = new SynchronizedDynamicGraph()
-
     val numActors = 5
-    val writers = (0 until numActors) map { n  =>
+    val writers = (0 until numActors) map { n =>
       actor {
         loop { receive {
           case (i: Int, j: Int) => graph.addEdge(i, j)
           case _: String => reply
         }}
       }
+    val writers = (0 until numActors) map { n  =>
+      ActorDSL.actor(system)(new Actor {
+        def receive = {
+              case (i: Int, j: Int) => graph.addEdge(i, j)
+              case _: String => sender ! "default"
+        }
+      })
     }
+
+    implicit val timeout = Timeout(36500 days)
 
     val numLoop = 15
     (1 to numLoop) foreach { i =>
@@ -102,7 +115,9 @@ class SynchronizedDynamicGraphSpec extends Specification {
       writers(n) ! pair
     }
 
-    (0 until numActors) foreach { writers(_) !? "stop" }
+    (0 until numActors) foreach { j =>
+      Await.result((writers(j)?"stop"), Duration.Inf)
+    }
 
     graph.nodeCount mustEqual (numLoop + 1)
     (2 to numLoop) foreach { i =>
@@ -119,27 +134,34 @@ class SynchronizedDynamicGraphSpec extends Specification {
   }
 
   "multipe threads add/remove edge simultaneously" in {
-    import akka.actor.Actor
-    import akka.actor.Actor._
+    import akka.actor._
+    import akka.actor.ActorDSL._
+    import akka.pattern.ask    
+    import akka.util.Timeout
+    import scala.concurrent._
+    import scala.concurrent.duration._
 
+    val system = ActorSystem("migration-system")
     val graph = new SynchronizedDynamicGraph()
+    val writer = ActorDSL.actor(system)(new Actor {
+      def receive = {
+            case (i: Int, j: Int) => {
+              graph.addEdge(i, j)
+            }
+            case _: String => sender ! "default"
+      }
+    })
 
-    val writer = actor {
-      loop { receive {
-        case (i: Int, j: Int) => {
-          graph.addEdge(i, j)
-        }
-        case _: String => reply
-      }}
-    }
-    val remover = actor {
-      loop { receive {
-        case (i: Int, j: Int) => {
-          graph.removeEdge(i, j)
-        }
-        case _: String => reply
-      }}
-    }
+    val remover = ActorDSL.actor(system)(new Actor {
+      def receive = {
+            case (i: Int, j: Int) => {
+              graph.removeEdge(i, j)
+            }
+            case _: String => sender ! "default"
+      }
+    })
+
+    implicit val timeout = Timeout(36500 days)
 
     val numLoop = 10
     (1 to numLoop) foreach { i =>
@@ -148,7 +170,7 @@ class SynchronizedDynamicGraphSpec extends Specification {
       val pair = (src, dest)
       writer ! pair
     }
-    writer !? "stop"
+    Await.result(writer ? "stop", Duration.Inf)
 
     (1 to numLoop) foreach { i =>
       val src = i
@@ -156,8 +178,8 @@ class SynchronizedDynamicGraphSpec extends Specification {
       val pair = (src, dest)
       remover ! pair
     }
-    remover !? "stop"
-
+    Await.result(remover ? "stop", Duration.Inf)
+    
     graph.nodeCount mustEqual 11
     (1 to numLoop + 1) foreach { i =>
       val node = graph.getNodeById(i).get
