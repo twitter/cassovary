9b36e610669e905cda903787fa047794daaefd3f
David Fuhry <dfuhry@twitter.com>
2013-11-02 13:11:12 -0700

    Additional tests for graph reading across {serial,parallel} x {unrenumbered,renumbered} dimensions

--- src/test/scala/com/twitter/cassovary/util/io/AdjacencyListGraphReaderSpec.scala
+++ src/test/scala/com/twitter/cassovary/util/io/AdjacencyListGraphReaderSpec.scala
@@ -15,7 +15,9 @@ package com.twitter.cassovary.util.io
 
 import com.twitter.cassovary.graph.DirectedGraph
 import com.twitter.cassovary.graph.LabeledNode
+import com.twitter.cassovary.graph.{NodeRenumberer,SequentialNodeRenumberer}
 import java.util.concurrent.Executors
+import com.google.common.util.concurrent.MoreExecutors
 import org.specs.Specification
 
 class AdjacencyListGraphReaderSpec extends Specification  {
@@ -42,10 +44,41 @@ class AdjacencyListGraphReaderSpec extends Specification  {
     nodeMap.keys.foreach { id => g.existsNodeId(id) mustBe true }
   }
 
+  /**
+   * Compares the nodes in a graph and those defined by the nodeMap (id -> ids of neighbors),
+   * remapping node ids thru nodeRenumberer, and ensures that these are equivalent
+   * @param g DirectedGraph
+   * @param nodeMap Map of node ids to ids of its neighbors
+   * @param nodeRenumberer a node renumberer
+   */
+  def nodeMapEqualsRenumbered(g:DirectedGraph, nodeMap: Map[Int, List[Int]], nodeRenumberer: NodeRenumberer) = {
+    g.foreach { node =>
+      nodeMap.contains(nodeRenumberer.nodeIdxToNodeId(node.id)) mustBe true
+      val neighbors = node.outboundNodes()
+      val nodesInMap = nodeMap(nodeRenumberer.nodeIdxToNodeId(node.id))
+      nodesInMap.foreach { i => neighbors.contains(nodeRenumberer.nodeIdToNodeIdx(i)) mustBe true }
+      neighbors.foreach { i => nodesInMap.contains(nodeRenumberer.nodeIdxToNodeId(i)) mustBe true }
+    }
+    nodeMap.keys.foreach { id => g.existsNodeId(nodeRenumberer.nodeIdToNodeIdx(id)) mustBe true }
+  }
+
+  /**
+   * Compares the labels of nodes in a graph to those defined by a label map.
+   */
   def labelListEquals(g: DirectedGraph, labelMap: Map[Int,Int]) = {
     g.foreach { node =>
       labelMap.contains(node.id) mustBe true
       node.asInstanceOf[LabeledNode].label mustBe labelMap(node.id)
+    }
+  }
+
+  /**
+   * Compares the labels of nodes in a graph to those defined by a label map.
+   */
+  def labelListEqualsRenumbered(g: DirectedGraph, labelMap: Map[Int,Int], nodeRenumberer: NodeRenumberer) = {
+    g.foreach { node =>
+      labelMap.contains(nodeRenumberer.nodeIdxToNodeId(node.id)) mustBe true
+      node.asInstanceOf[LabeledNode].label mustBe labelMap(nodeRenumberer.nodeIdxToNodeId(node.id))
     }
   }
 
@@ -73,7 +106,31 @@ class AdjacencyListGraphReaderSpec extends Specification  {
 
   }
 
-  "AdjacencyListReader labeled" should {
+  "AdjacencyListReader serial labeled" should {
+
+    doBefore{
+      graph = new AdjacencyListGraphReader("src/test/resources/graphs/", "toy_6nodes-2_labeled", new LabeledVertexReaderFactory()) {
+          override val executorService = MoreExecutors.sameThreadExecutor()
+        }.toSharedArrayBasedDirectedGraph()
+    }
+
+    "provide the correct graph properties" in {
+      graph.nodeCount mustBe 6
+      graph.edgeCount mustBe 11L
+      graph.maxNodeId mustBe 15
+    }
+
+    "contain the right nodes and edges" in {
+      nodeMapEquals(graph, nodeMap)
+    }
+
+    "contain the right labels" in {
+      labelListEquals(graph, labelMap)
+    }
+
+  }
+
+  "AdjacencyListReader parallel labeled" should {
 
     doBefore{
       graph = new AdjacencyListGraphReader("src/test/resources/graphs/", "toy_6nodes_labeled_adj", new LabeledVertexReaderFactory()) {
@@ -96,5 +153,58 @@ class AdjacencyListGraphReaderSpec extends Specification  {
     }
 
   }
+
+  "AdjacencyListReader serial labeled renumbered" should {
+
+    var seqRenumberer = new SequentialNodeRenumberer()
+
+    doBefore{
+      graph = new AdjacencyListGraphReader("src/test/resources/graphs/", "toy_6nodes-2_labeled", new LabeledVertexReaderFactory(), seqRenumberer) {
+          override val executorService = MoreExecutors.sameThreadExecutor()
+        }.toSharedArrayBasedDirectedGraph()
+    }
+
+    "provide the correct graph properties" in {
+      graph.nodeCount mustBe 6
+      graph.edgeCount mustBe 11L
+      graph.maxNodeId mustBe 5  // Zero-based; node ids are 0 thru 5.
+    }
+
+    "contain the right renumbered nodes and edges" in {
+      nodeMapEqualsRenumbered(graph, nodeMap, seqRenumberer)
+    }
+
+    "contain the right renumbered labels" in {
+      labelListEqualsRenumbered(graph, labelMap, seqRenumberer)
+    }
+
+  }
+
+  "AdjacencyListReader parallel labeled renumbered" should {
+
+    var seqRenumberer = new SequentialNodeRenumberer()
+
+    doBefore{
+      graph = new AdjacencyListGraphReader("src/test/resources/graphs/", "toy_6nodes_labeled_adj", new LabeledVertexReaderFactory(), seqRenumberer) {
+          override val executorService = Executors.newFixedThreadPool(2)
+        }.toSharedArrayBasedDirectedGraph()
+    }
+
+    "provide the correct graph properties" in {
+      graph.nodeCount mustBe 6
+      graph.edgeCount mustBe 11L
+      graph.maxNodeId mustBe 5  // Zero-based; node ids are 0 thru 5.
+    }
+
+    "contain the right renumbered nodes and edges" in {
+      nodeMapEqualsRenumbered(graph, nodeMap, seqRenumberer)
+    }
+
+    "contain the right renumbered labels" in {
+      labelListEqualsRenumbered(graph, labelMap, seqRenumberer)
+    }
+
+  }
+
 
 }
