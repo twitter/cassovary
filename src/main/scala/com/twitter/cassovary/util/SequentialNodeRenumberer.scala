501be5d770b04983cfd0a43606890404c4b3abd5
David Fuhry <dfuhry@twitter.com>
2013-11-26 17:10:12 -0800

    Update SequentialNodeRenumberer to maintain parallel externalToInternal, and internalToExternal maps to ensure consistency.

--- src/main/scala/com/twitter/cassovary/util/SequentialNodeRenumberer.scala
+++ src/main/scala/com/twitter/cassovary/util/SequentialNodeRenumberer.scala
@@ -13,7 +13,7 @@
  */
 package com.twitter.cassovary.util
 
-import scala.collection.mutable.HashMap
+import scala.collection.mutable.{ArrayBuffer,HashMap}
 
 /*
  * Renumbers node id values read from source file to internal sequentially increasing id value.
@@ -22,19 +22,30 @@ import scala.collection.mutable.HashMap
 class SequentialNodeRenumberer extends NodeRenumberer {
 
   val externalToInternalMap = HashMap[Int,Int]()
-  var lazyInternalToExternalMap = Array[Int]()
+  var internalToExternalMap = ArrayBuffer[Int]()
 
   def externalToInternal(externalNodeId: Int): Int =
     this.synchronized {
-      externalToInternalMap.getOrElseUpdate(externalNodeId, externalToInternalMap.size)
+      val internalNodeId = externalToInternalMap.getOrElseUpdate(externalNodeId, externalToInternalMap.size)
+
+      // Note: Assertions are O(1) consistency checks which can be commented out for performance.
+      // When loaded correctly we should not be seeing internal ids beyond length of externalToInternalMap.
+      assert(externalToInternalMap.size > internalNodeId)
+      // InternalNodeId should be no larger than internalToExternalMap.
+      assert(internalToExternalMap.size >= internalNodeId)
+
+      if (internalToExternalMap.size == internalNodeId) {
+        internalToExternalMap += externalNodeId
+      }
+
+      // The internal id should be assigned to the correct external id.
+      assert(internalToExternalMap(internalNodeId) == externalNodeId)
+
+      internalNodeId
     }
 
   def internalToExternal(internalNodeId: Int): Int = {
-    if (lazyInternalToExternalMap.isEmpty) {
-      lazyInternalToExternalMap = new Array[Int](externalToInternalMap.size)
-      externalToInternalMap.foreach { case (externalNodeId, internalNodeId) => lazyInternalToExternalMap(internalNodeId) = externalNodeId }
-    }
-    lazyInternalToExternalMap(internalNodeId)
+    internalToExternalMap(internalNodeId)
   }
 
 }
