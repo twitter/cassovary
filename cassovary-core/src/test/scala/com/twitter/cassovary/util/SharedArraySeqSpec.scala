02bcb35b53be2308e43386c637c4260dc1a48f4d
Bartek Kalinka <bartkali@hotmail.com>
2014-12-30 12:51:04 +0100

    SharedArraySeqSpec correct package

--- cassovary-core/src/test/scala/com/twitter/cassovary/util/SharedArraySeqSpec.scala
+++ cassovary-core/src/test/scala/com/twitter/cassovary/util/SharedArraySeqSpec.scala
@@ -11,9 +11,8 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package com.twitter.cassovary.util.io
+package com.twitter.cassovary.util
 
-import com.twitter.cassovary.util.SharedArraySeq
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.WordSpec
 
