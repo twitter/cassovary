e0b6126017860bc326236b8e4a4e7965f85bffa4
Bartek Kalinka <bartkali@hotmail.com>
2015-01-06 15:22:51 +0100

    SmallBoundedPriorityQueueSpec no conversions to list

--- cassovary-core/src/test/scala/com/twitter/cassovary/util/SmallBoundedPriorityQueueSpec.scala
+++ cassovary-core/src/test/scala/com/twitter/cassovary/util/SmallBoundedPriorityQueueSpec.scala
@@ -34,8 +34,8 @@ class SmallBoundedPriorityQueueSpec extends WordSpec with ShouldMatchers {
     "empty" should {
       "return empty list" in {
         val q = emptyQueue[Int](3)
-        q.top(0).toList shouldEqual List()
-        q.top(1).toList shouldEqual List()
+        q.top(0) shouldEqual Seq()
+        q.top(1) shouldEqual Seq()
       }
     }
 
@@ -43,11 +43,11 @@ class SmallBoundedPriorityQueueSpec extends WordSpec with ShouldMatchers {
       "add elements" in {
         val q = emptyQueue[Int](3)
         q += 4
-        q.top(1).toList shouldEqual List(4)
+        q.top(1) shouldEqual Seq(4)
         q += 3
-        q.top(2).toList shouldEqual List(4, 3)
+        q.top(2) shouldEqual Seq(4, 3)
         q += 30
-        q.top(3).toList shouldEqual List(30, 4, 3)
+        q.top(3) shouldEqual Seq(30, 4, 3)
       }
     }
 
@@ -55,16 +55,16 @@ class SmallBoundedPriorityQueueSpec extends WordSpec with ShouldMatchers {
       "substitute lowest element correctly" in {
         val q = fullQueue[Int](List(4, 3, 30))
         q += 10
-        q.top(3).toList shouldEqual List(30, 10, 4)
+        q.top(3) shouldEqual Seq(30, 10, 4)
         q += 100
         q += 1
         q += 10
-        q.top(3).toList shouldEqual List(100, 30, 10)
+        q.top(3) shouldEqual Seq(100, 30, 10)
       }
 
       "return correct number of elements" in {
         val q = fullQueue[Int](List(4, 3, 30))
-        q.top(1).toList shouldEqual List(30)
+        q.top(1) shouldEqual Seq(30)
       }
     }
 
@@ -75,7 +75,7 @@ class SmallBoundedPriorityQueueSpec extends WordSpec with ShouldMatchers {
         q.size shouldEqual 0
         q += 1
         q += 10
-        q.top(2).toList shouldEqual List(10, 1)
+        q.top(2) shouldEqual Seq(10, 1)
       }
     }
   }
@@ -87,11 +87,11 @@ class SmallBoundedPriorityQueueSpec extends WordSpec with ShouldMatchers {
         val q = emptyQueue[String](5)
         q += "ab"
         q += "cd"
-        q.top(2).toList shouldEqual List("cd", "ab")
+        q.top(2) shouldEqual Seq("cd", "ab")
         q += "ar"
         q += "be"
         q += "fd"
-        q.top(5).toList shouldEqual List("fd", "cd", "be", "ar", "ab")
+        q.top(5) shouldEqual Seq("fd", "cd", "be", "ar", "ab")
       }
     }
 
@@ -100,12 +100,12 @@ class SmallBoundedPriorityQueueSpec extends WordSpec with ShouldMatchers {
         val q = fullQueue[String](List("fd", "cd", "be", "ar", "ab"))
         q += "zz"
         q += "xx"
-        q.top(5).toList shouldEqual List("zz", "xx", "fd", "cd", "be")
+        q.top(5) shouldEqual Seq("zz", "xx", "fd", "cd", "be")
       }
 
       "return correct number of elements" in {
         val q = fullQueue[String](List("ar", "ab", "fd", "cd", "be"))
-        q.top(3).toList shouldEqual List("fd", "cd", "be")
+        q.top(3) shouldEqual Seq("fd", "cd", "be")
       }
     }
   }
