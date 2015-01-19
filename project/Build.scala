ca19d5e8b7723609de56a919ae2a7c23e67f108f
Anish Shah <shah.anish07@gmail.com>
2015-01-19 20:25:17 +0530

    fixed indentation

--- project/Build.scala
+++ project/Build.scala
@@ -46,29 +46,29 @@ object Cassovary extends Build {
     publishMavenStyle := true,
     pomExtra := (
       <url>http://twitter.com/cassovary</url>
-        <licenses>
-          <license>
-            <name>Apache 2</name>
-            <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-            <distribution>repo</distribution>
-            <comments>A business-friendly OSS license</comments>
-          </license>
-        </licenses>
-        <scm>
-          <url>git@github.com:twitter/cassovary.git</url>
-          <connection>scm:git:git@github.com:twitter/cassovary.git</connection>
-        </scm>
-        <developers>
-          <developer><id>pankaj</id><name>Pankaj Gupta</name><url>https://twitter.com/pankaj</url></developer>
-          <developer><id>dongwang218</id><name>Dong Wang</name><url>https://twitter.com/dongwang218</url></developer>
-          <developer><id>tao</id><name>Tao Tao</name><url>https://twitter.com/tao</url></developer>
-          <developer><id>johnsirois</id><name>John Sirois</name><url>https://twitter.com/johnsirois</url></developer>
-          <developer><id>aneeshs</id><name>Aneesh Sharma</name><url>https://twitter.com/aneeshs</url></developer>
-          <developer><id>ashishgoel</id><name>Ashish Goel</name><url>https://twitter.com/ashishgoel</url></developer>
-          <developer><id>4ad</id><name>Mengqiu Wang</name><url>https://twitter.com/4ad</url></developer>
-          <developer><id>ningliang</id><name>Ning Liang</name><url>https://twitter.com/ningliang</url></developer>
-          <developer><id>ajeet</id><name>Ajeet Grewal</name><url>https://twitter.com/ajeet</url></developer>
-        </developers>),
+      <licenses>
+        <license>
+          <name>Apache 2</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+          <distribution>repo</distribution>
+          <comments>A business-friendly OSS license</comments>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:twitter/cassovary.git</url>
+        <connection>scm:git:git@github.com:twitter/cassovary.git</connection>
+      </scm>
+      <developers>
+        <developer><id>pankaj</id><name>Pankaj Gupta</name><url>https://twitter.com/pankaj</url></developer>
+        <developer><id>dongwang218</id><name>Dong Wang</name><url>https://twitter.com/dongwang218</url></developer>
+        <developer><id>tao</id><name>Tao Tao</name><url>https://twitter.com/tao</url></developer>
+        <developer><id>johnsirois</id><name>John Sirois</name><url>https://twitter.com/johnsirois</url></developer>
+        <developer><id>aneeshs</id><name>Aneesh Sharma</name><url>https://twitter.com/aneeshs</url></developer>
+        <developer><id>ashishgoel</id><name>Ashish Goel</name><url>https://twitter.com/ashishgoel</url></developer>
+        <developer><id>4ad</id><name>Mengqiu Wang</name><url>https://twitter.com/4ad</url></developer>
+        <developer><id>ningliang</id><name>Ning Liang</name><url>https://twitter.com/ningliang</url></developer>
+        <developer><id>ajeet</id><name>Ajeet Grewal</name><url>https://twitter.com/ajeet</url></developer>
+      </developers>),
     publishTo <<= version { (v: String) =>
       val nexus = "https://oss.sonatype.org/"
       if (v.trim.endsWith("SNAPSHOT"))
@@ -84,24 +84,24 @@ object Cassovary extends Build {
     settings = Project.defaultSettings ++ sharedSettings ++ sonatypeSettings
   ).aggregate(
       cassovaryCore, cassovaryExamples, cassovaryBenchmarks, cassovaryServer
-    )
+  )
 
   lazy val cassovaryCore = Project(
     id = "cassovary-core",
     base = file("cassovary-core"),
     settings = Project.defaultSettings ++ sharedSettings ++ sonatypeSettings
   ).settings(
-      name := "cassovary-core"
-    )
+    name := "cassovary-core"
+  )
 
   lazy val cassovaryExamples = Project(
     id = "cassovary-examples",
     base = file("cassovary-examples"),
     settings = Project.defaultSettings ++ sharedSettings
   ).settings(
-      name := "cassovary-examples",
-      libraryDependencies += fastUtilsDependency
-    ).dependsOn(cassovaryCore)
+    name := "cassovary-examples",
+    libraryDependencies += fastUtilsDependency
+  ).dependsOn(cassovaryCore)
 
   lazy val cassovaryBenchmarks = Project(
     id = "cassovary-benchmarks",
@@ -113,7 +113,7 @@ object Cassovary extends Build {
         fastUtilsDependency,
         "com.twitter" %% "util-app" % "6.12.1"
       )
-    ).dependsOn(cassovaryCore)
+  ).dependsOn(cassovaryCore)
 
   lazy val cassovaryServer = Project(
     id = "cassovary-server",
@@ -127,6 +127,6 @@ object Cassovary extends Build {
         "com.twitter" %% "finagle-ostrich4" % "6.20.0",
         "com.twitter" %% "twitter-server" % "1.9.0"
       )
-    ).dependsOn(cassovaryCore)
+  ).dependsOn(cassovaryCore)
 
 }
