9a706a19521807ef62366d172744d20bb38a72d7
Ben McKown <bgmckown@gmail.com>
2015-01-28 10:19:43 -0600

    Add docs

--- cassovary-core/src/main/scala/com/twitter/cassovary/algorithms/centrality/DegreeCentrality.scala
+++ cassovary-core/src/main/scala/com/twitter/cassovary/algorithms/centrality/DegreeCentrality.scala
@@ -1,14 +1,31 @@
+/*
+ * Copyright 2014 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.twitter.cassovary.algorithms.centrality
 
 import com.twitter.cassovary.graph.DirectedGraph
 
-/**
- * Created by bmckown on 1/28/15.
- */
-
 sealed abstract class DegreeCentrality(graph: DirectedGraph) extends AbstractCentrality(graph)
 
 object InDegreeCentrality {
+
+  /**
+   * This method determines the indegree centrality of each node in the graph.  Currently, we only support
+   * calculating the IDC for each member of the graph and not for the graph on the whole.  Although this
+   * functionality would not be terribly difficult to add.
+   * @param graph A DirectedGraph instance
+   * @return
+   */
   def apply(graph: DirectedGraph): Array[Double] = {
     val idc = new InDegreeCentrality(graph)
     idc.run
@@ -16,6 +33,14 @@ object InDegreeCentrality {
 }
 
 object OutDegreeCentrality {
+
+  /**
+   * This method determines the outdegree centrality of each node in the graph.  Currently, we only support
+   * calculating the ODC for each member of the graph and not for the graph on the whole.  Although this
+   * functionality would not be terribly difficult to add.
+   * @param graph A DirectedGraph instance
+   * @return
+   */
   def apply(graph: DirectedGraph): Array[Double] = {
     val odc = new OutDegreeCentrality(graph)
     odc.run
@@ -23,8 +48,12 @@ object OutDegreeCentrality {
 }
 
 private class InDegreeCentrality(graph: DirectedGraph) extends AbstractCentrality(graph) {
-  def run: Array[Double] = {
 
+  /**
+   * Run the indegree centrality calculation for the graph.
+   * @return
+   */
+  def run: Array[Double] = {
     graph.foreach { node =>
       centrality(node.id) = node.inboundCount
     }
@@ -33,6 +62,11 @@ private class InDegreeCentrality(graph: DirectedGraph) extends AbstractCentralit
 }
 
 private class OutDegreeCentrality(graph: DirectedGraph) extends AbstractCentrality(graph) {
+
+  /**
+   * Run the outdegree centrality calculation for the graph.
+   * @return
+   */
   def run: Array[Double] = {
     graph.foreach { node =>
       centrality(node.id) = node.outboundCount
