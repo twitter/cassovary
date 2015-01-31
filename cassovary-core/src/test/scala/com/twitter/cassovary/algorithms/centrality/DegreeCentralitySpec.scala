bd9472b5acab459e022dbb790660695d42148fc5
Ben McKown <bgmckown@gmail.com>
2015-01-31 11:33:25 -0600

    Update tests

--- cassovary-core/src/test/scala/com/twitter/cassovary/algorithms/centrality/DegreeCentralitySpec.scala
+++ cassovary-core/src/test/scala/com/twitter/cassovary/algorithms/centrality/DegreeCentralitySpec.scala
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.twitter.cassovary.algorithms.centrality
 
 import com.twitter.cassovary.graph._
@@ -18,6 +31,10 @@ class DegreeCentralitySpec extends WordSpec with Matchers {
       centrality(0)  shouldEqual 0.0
       centrality(1)  shouldEqual 0.0
       centrality(11) shouldEqual 0.2857142857142857
+      centrality(12) shouldEqual 0.42857142857142855
+      centrality(13) shouldEqual 0.14285714285714285
+      centrality(14) shouldEqual 0.42857142857142855
+      centrality(15) shouldEqual 0.14285714285714285
     }
 
     "return out-degree centrality values that are integer valued" in {
@@ -26,6 +43,10 @@ class DegreeCentralitySpec extends WordSpec with Matchers {
       centrality(0)  shouldEqual 0.0
       centrality(1)  shouldEqual 0.0
       centrality(11) shouldEqual 2.0
+      centrality(12) shouldEqual 1.0
+      centrality(13) shouldEqual 2.0
+      centrality(14) shouldEqual 1.0
+      centrality(15) shouldEqual 2.0
     }
   }
 }
